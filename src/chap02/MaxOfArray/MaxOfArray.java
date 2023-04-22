7700514a9e325d2d2a1cd54b9ad3634c90a038a6
DY <kks40085647@gmail.com>
2023-04-22 10:21:01 +0900

    알고리즘 2장

---
+++ src/chap02/MaxOfArray/MaxOfArray.java
@@ -0,0 +1,11 @@
+package src.chap02.MaxOfArray;
+
+public class MaxOfArray {
+    //배열 a의 최댓값을 구하여 반환합니다.
+    static int maxOf(int[] a){
+        int max = a[0];
+        for (int i = 0; i < a.length; i++) {
+
+        }
+    }
+}
